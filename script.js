9a985a4044d36a458e5c6de2d599eb8ae1e835af
HRITHIK VERMA <97848036+vermahrithik@users.noreply.github.com>
2022-10-02 21:41:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ var c = gsap.timeline();
 c
   .to("#ll",{
   y: 1080,
-  duration: 4,
+  duration: 3,
   ease: Expo.easeInOut,
 })
 .to("#lr",{
   y: -1080,
-  Delay:4,
-  duration: 4,
+  Delay:3,
+  duration: 3,
   ease: Expo.easeInOut,
 },0)
 .to("#wall",{
   y:-1080,
-  duration:4,
+  duration:2,
   ease: Expo.easeInOut,
 },+2)
 .from("#name,#menu,#options",{
     y:10,
     duration:1,
-    stagger:1,
+    stagger:0.25,
     ease:Expo.easeInOut,
     opacity:0,
     delay:-1
@@ -34,42 +34,40 @@ c
 })
 .from("#tpr",{
     x:-50,
-    duration:1,
+    duration:0.5,
     ease:Expo.easeInOut,
     opacity:0,
-    delay:-0.5
+    delay:-0.4
 })
 .from("#bpr",{
     x:-50,
-    duration:1,
-    // stagger:1,
+    duration:0.5,
     ease:Expo.easeInOut,
     opacity:0,
-    delay:0
+    delay:-0.2
 })
 .from("#bpl",{
     x:-50,
-    duration:1,
-    // stagger:1,
+    duration:0.5,
     ease:Expo.easeInOut,
     opacity:0,
-    delay:-0.5
+    delay:-0.2
 })
 .from("#click,#model",{
     x:-300,
-    duration:1,
+    duration:0.5,
     stagger:0.5,
     ease:Expo.easeInOut,
     opacity:0,
-    delay:-0.5,
+    delay:-0.25
 })
 .from("#se,.options",{
     y:300,
-    duration:1,
-    stagger:0.5,
+    duration:0.25,
+    stagger:0.25,
     ease:Expo.easeInOut,
     opacity:0,
-    delay:-0.5,
+    delay:-0.25,
 })
 .from("#dp,.loine,.word",{
     x:-100,
